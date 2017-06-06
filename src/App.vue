301ba879690ce6e05dfe203884783a9c7b2ba6b7
ssyyjj88 <sunyujuan@syjdeMacBook-Pro.local>
2017-06-06 11:00:04 +0800

    no message

--- src/App.vue
+++ src/App.vue
@@ -1,13 +1,39 @@
 <template>
   <div id="app">
-    <img src="./assets/logo.png">
-    <router-view></router-view>
+    <!--<h1>{{ title }}</h1>-->
+    <!--<h1 v-text="title"></h1>-->
+    <h1 v-html="title"></h1> <!-- title: '<span>?</span>this is a todo list'-->
+  	<ul>
+  		<li v-for="item in items">
+  			{{item.label}}
+  		</li>
+  	</ul>
   </div>
 </template>
 
 <script>
 export default {
-  name: 'app'
+// name: 'app'
+  data: function () {
+    return {
+      title: 'this is a todo list',
+      items: [
+        {
+          label: 'coding',
+          inFinished: false
+        },
+        {
+          label: 'walking',
+          inFinished: true
+        }
+      ]
+    }
+  }
+// data () {
+//   return {
+//   msg: 'Welcome to Your Vue.js App'
+//  }
+// }
 }
 </script>
 
